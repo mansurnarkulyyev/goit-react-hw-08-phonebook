3fe4d63afeaa40388a169bc7a99e5612898d3992
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-08-07 18:32:50 +0300

    Updates

---
+++ static/js/93.c8132c66.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[93],{3093:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var r=o(3504),t=o(184),c=function(){return(0,t.jsx)("main",{children:(0,t.jsxs)("div",{className:"container",children:[(0,t.jsx)("h2",{children:"Page not found"}),(0,t.jsx)(r.rU,{to:"/",children:"To home page"})]})})}}}]);
+//# sourceMappingURL=93.c8132c66.chunk.js.map
